c56e75814d229bb3c58ece4d67540c0e3b7174cf
Leonardo Azaña <leonardoazliendo19@outlook.com>
2026-07-05 22:13:03 -0500

    Arduino comunicacion con pc

--- View/Client.h
+++ View/Client.h
@@ -3,6 +3,7 @@
 
 using namespace System::IO;
 using namespace System::Data::SqlClient;
+using namespace ModelMixer;
 
 namespace View {
 
@@ -96,10 +97,9 @@ namespace View {
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label2->ForeColor = System::Drawing::Color::Transparent;
-			this->label2->Location = System::Drawing::Point(39, 119);
-			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label2->Location = System::Drawing::Point(29, 97);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(217, 25);
+			this->label2->Size = System::Drawing::Size(175, 20);
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"Nuestras opciones son:";
 			// 
@@ -110,10 +110,9 @@ namespace View {
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label3->ForeColor = System::Drawing::Color::Transparent;
-			this->label3->Location = System::Drawing::Point(96, 186);
-			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label3->Location = System::Drawing::Point(72, 151);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(76, 25);
+			this->label3->Size = System::Drawing::Size(60, 20);
 			this->label3->TabIndex = 2;
 			this->label3->Text = L"- Mojito";
 			// 
@@ -124,10 +123,9 @@ namespace View {
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label4->ForeColor = System::Drawing::Color::Transparent;
-			this->label4->Location = System::Drawing::Point(217, 186);
-			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label4->Location = System::Drawing::Point(163, 151);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(120, 25);
+			this->label4->Size = System::Drawing::Size(95, 20);
 			this->label4->TabIndex = 3;
 			this->label4->Text = L"- Cuba Libre";
 			// 
@@ -138,27 +136,28 @@ namespace View {
 			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label5->ForeColor = System::Drawing::Color::Transparent;
-			this->label5->Location = System::Drawing::Point(361, 186);
-			this->label5->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label5->Location = System::Drawing::Point(271, 151);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(77, 25);
+			this->label5->Size = System::Drawing::Size(61, 20);
 			this->label5->TabIndex = 4;
 			this->label5->Text = L"- Daikiri";
 			// 
 			// comboBoxBebida
 			// 
 			this->comboBoxBebida->FormattingEnabled = true;
-			this->comboBoxBebida->Location = System::Drawing::Point(311, 263);
+			this->comboBoxBebida->Location = System::Drawing::Point(233, 214);
+			this->comboBoxBebida->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->comboBoxBebida->Name = L"comboBoxBebida";
-			this->comboBoxBebida->Size = System::Drawing::Size(121, 24);
+			this->comboBoxBebida->Size = System::Drawing::Size(92, 21);
 			this->comboBoxBebida->TabIndex = 6;
 			// 
 			// comboBoxTamano
 			// 
 			this->comboBoxTamano->FormattingEnabled = true;
-			this->comboBoxTamano->Location = System::Drawing::Point(311, 346);
+			this->comboBoxTamano->Location = System::Drawing::Point(233, 281);
+			this->comboBoxTamano->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->comboBoxTamano->Name = L"comboBoxTamano";
-			this->comboBoxTamano->Size = System::Drawing::Size(121, 24);
+			this->comboBoxTamano->Size = System::Drawing::Size(92, 21);
 			this->comboBoxTamano->TabIndex = 7;
 			// 
 			// label6
@@ -168,10 +167,9 @@ namespace View {
 			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label6->ForeColor = System::Drawing::Color::Transparent;
-			this->label6->Location = System::Drawing::Point(83, 262);
-			this->label6->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label6->Location = System::Drawing::Point(62, 213);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(179, 25);
+			this->label6->Size = System::Drawing::Size(143, 20);
 			this->label6->TabIndex = 8;
 			this->label6->Text = L"Seleccione bebida:";
 			this->label6->Click += gcnew System::EventHandler(this, &Client::label6_Click);
@@ -183,10 +181,9 @@ namespace View {
 			this->label7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label7->ForeColor = System::Drawing::Color::Transparent;
-			this->label7->Location = System::Drawing::Point(83, 346);
-			this->label7->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label7->Location = System::Drawing::Point(62, 281);
 			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(91, 25);
+			this->label7->Size = System::Drawing::Size(71, 20);
 			this->label7->TabIndex = 9;
 			this->label7->Text = L"Tamano:";
 			// 
@@ -195,9 +192,10 @@ namespace View {
 			this->checkBoxHielo->AutoSize = true;
 			this->checkBoxHielo->BackColor = System::Drawing::Color::Transparent;
 			this->checkBoxHielo->ForeColor = System::Drawing::Color::Transparent;
-			this->checkBoxHielo->Location = System::Drawing::Point(90, 414);
+			this->checkBoxHielo->Location = System::Drawing::Point(68, 336);
+			this->checkBoxHielo->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->checkBoxHielo->Name = L"checkBoxHielo";
-			this->checkBoxHielo->Size = System::Drawing::Size(110, 20);
+			this->checkBoxHielo->Size = System::Drawing::Size(88, 17);
 			this->checkBoxHielo->TabIndex = 10;
 			this->checkBoxHielo->Text = L"Agregar hielo";
 			this->checkBoxHielo->UseVisualStyleBackColor = false;
@@ -205,9 +203,10 @@ namespace View {
 			// 
 			// btnPreparar
 			// 
-			this->btnPreparar->Location = System::Drawing::Point(280, 404);
+			this->btnPreparar->Location = System::Drawing::Point(210, 328);
+			this->btnPreparar->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->btnPreparar->Name = L"btnPreparar";
-			this->btnPreparar->Size = System::Drawing::Size(178, 39);
+			this->btnPreparar->Size = System::Drawing::Size(134, 32);
 			this->btnPreparar->TabIndex = 11;
 			this->btnPreparar->Text = L"PREPARAR BEBIDA";
 			this->btnPreparar->UseVisualStyleBackColor = true;
@@ -216,11 +215,13 @@ namespace View {
 			// pictureBox9
 			// 
 			this->pictureBox9->BackColor = System::Drawing::Color::Transparent;
-			this->pictureBox9->Location = System::Drawing::Point(464, 206);
+			this->pictureBox9->Location = System::Drawing::Point(348, 167);
+			this->pictureBox9->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->pictureBox9->Name = L"pictureBox9";
-			this->pictureBox9->Size = System::Drawing::Size(417, 237);
+			this->pictureBox9->Size = System::Drawing::Size(313, 193);
 			this->pictureBox9->TabIndex = 14;
 			this->pictureBox9->TabStop = false;
+			this->pictureBox9->Click += gcnew System::EventHandler(this, &Client::pictureBox9_Click);
 			// 
 			// label1
 			// 
@@ -229,20 +230,21 @@ namespace View {
 			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9, System::Drawing::FontStyle::Italic, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->label1->ForeColor = System::Drawing::Color::White;
-			this->label1->Location = System::Drawing::Point(25, 21);
+			this->label1->Location = System::Drawing::Point(19, 17);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(128, 20);
+			this->label1->Size = System::Drawing::Size(103, 15);
 			this->label1->TabIndex = 15;
 			this->label1->Text = L"Sistema Mixer v1.0";
 			// 
 			// Client
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ControlDark;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
-			this->ClientSize = System::Drawing::Size(893, 546);
+			this->ClientSize = System::Drawing::Size(670, 444);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->pictureBox9);
 			this->Controls->Add(this->btnPreparar);
@@ -256,7 +258,6 @@ namespace View {
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
 			this->ImeMode = System::Windows::Forms::ImeMode::Disable;
-			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"Client";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Client";
@@ -350,5 +351,7 @@ namespace View {
 
 		}
 	}
+private: System::Void pictureBox9_Click(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
